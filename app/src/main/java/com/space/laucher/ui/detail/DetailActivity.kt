a53bd116f78ebc3c29080735a6403a1b51f7c3bb
Disf <ponomaREG>
2020-09-06 19:16:18 +0300

    google map intent

--- app/src/main/java/com/space/laucher/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/space/laucher/ui/detail/DetailActivity.kt
@@ -2,13 +2,16 @@ package com.space.laucher.ui.detail
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import com.space.laucher.R
-import com.space.laucher.databinding.ActivityDetailBinding
+import com.space.laucher.extensions.gone
 import com.space.laucher.extensions.loadPhoto
 import com.space.laucher.model.Launch
 import com.space.laucher.sample.DataBindingActivity
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.activity_detail.view.*
+import java.lang.Exception
 
 class DetailActivity : DataBindingActivity() {
 //    private val binding:ActivityDetailBinding by binding(R.layout.activity_detail)
@@ -40,5 +43,25 @@ class DetailActivity : DataBindingActivity() {
         ptv_countryCode.setTextOfRightTextView(launchExtra?.lsp?.countryCode)
         val imageURL = launchExtra?.rocket?.imageURL
         if(imageURL != null)detail_imageOfRocket.loadPhoto(imageURL)
+
+        var latitude:String? = null
+        var longitude:String? = null
+        try {
+            latitude = launchExtra?.location?.pads?.get(0)?.latitude.toString()
+            longitude = launchExtra?.location?.pads?.get(0)?.longitude.toString()
+            if ((latitude == null) or (longitude == null)) detail_showOnMap.gone(true)
+        }catch (e:Exception) {
+            detail_showOnMap.gone(true)
+        }
+
+        detail_showOnMap.setOnClickListener {
+            val gmmIntentUri: Uri = Uri.parse("geo:$latitude,$longitude")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            if (mapIntent.resolveActivity(packageManager) != null) {
+                startActivity(mapIntent)
+            }
+        }
     }
+
 }
